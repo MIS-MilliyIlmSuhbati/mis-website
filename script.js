1eb1129d8f111f0d6e672ff84e6b5547a3c34426
MIS-uz <jbahodir770@gmail.com>
2025-12-22 16:24:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("active");
+}
+
+window.addEventListener("scroll",()=>{
+  document.getElementById("header")
+    .classList.toggle("shrink",window.scrollY>80);
+});
 
